044bc166e86beefcce02d7bea52dd489504f63b9
Ulrich Zurucker <ulizurucker@googlemail.com>
2012-02-11 15:06:43 +0100

    launch Safari when not open and prevent from having 1000's of windows or tabs open

--- google_white_noise.rb
+++ google_white_noise.rb
@@ -1,8 +1,10 @@
+`osascript -e 'tell application "Safari" to activate'`
+
 dictionary = File.open("/usr/share/dict/web2").readlines
 wc = dictionary.count
 
 loop do
   word = dictionary[rand wc]
-  `osascript -e 'tell application \"Safari\" to search the web for "#{word}"'`
+  `osascript -e 'tell application \"Safari\" to search the web for "#{word}" in document 1'`
   sleep(42 - rand(24))
 end
